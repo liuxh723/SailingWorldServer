36114b2417ffa6141f41ca9ad7007219679caf9c
liuxh723 <86610076@qq.com>
2018-05-09 22:29:45 +0800

    up

--- scripts/base/City.py
+++ scripts/base/City.py
@@ -6,16 +6,16 @@ from KBEDebug import *
 class City(KBEngine.Proxy):
     def __init__(self):
         KBEngine.Proxy.__init__(self)
-        DEBUG_MSG("City::__init__: city %i.name %s entityID=%i" % (self.CityID,self.CityName,self.id))	
-        #self.initProperty()
+        DEBUG_MSG("City::__init__: city %i name: %s entityID=%i" % (self.CityID, self.CityName, self.id))
+        self.initProperty()
 
     def initProperty(self):
         CityID = self.CityID
-        self.CityName = d_city[CityID]["CityName"]
-        self.CityType = d_city[CityID]["CityType"]
-        self.CityCountry = d_city[CityID]["CityCountry"]
-        self.BusinessDvelopment = d_city[CityID]["BusinessDvelopment"]
-        self.MilitaryDvelopment = d_city[CityID]["MilitaryDvelopment"]
+        self.CityName = d_city.datas[CityID]["CityName"]
+        self.CityType = d_city.datas[CityID]["CityType"]
+        self.CityCountry = d_city.datas[CityID]["CityCountry"]
+        self.BusinessDvelopment = d_city.datas[CityID]["BusinessDvelopment"]
+        self.MilitaryDvelopment = d_city.datas[CityID]["MilitaryDvelopment"]
 
     def onTimer(self, id, userArg):
         """
