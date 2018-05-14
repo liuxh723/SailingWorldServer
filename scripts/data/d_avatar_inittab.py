9e93b2e1bf0b7e5027c2c923cd8deceb673be10f
liuxh723 <86610076@qq.com>
2018-05-14 22:58:49 +0800

    avatar init

--- scripts/data/d_avatar_inittab.py
+++ scripts/data/d_avatar_inittab.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 datas={1: {'spawnPos': (771.5861, 211.0021, 776.5501), 'country': 1, 'spaceUType': 1, 'level': 0, 'moveSpeed': 60, 'gold': 0, 'exp': 0, 'spawnYaw': 0}, 2: {'spawnPos': (771.5861, 211.0021, 776.5501), 'country': 2, 'spaceUType': 1, 'level': 0, 'moveSpeed': 60, 'gold': 0, 'exp': 0, 'spawnYaw': 0}, 3: {'spawnPos': (771.5861, 211.0021, 776.5501), 'country': 3, 'spaceUType': 1, 'level': 0, 'moveSpeed': 60, 'gold': 0, 'exp': 0, 'spawnYaw': 0}, 4: {'spawnPos': (771.5861, 211.0021, 776.5501), 'country': 4, 'spaceUType': 1, 'level': 0, 'moveSpeed': 60, 'gold': 0, 'exp': 0, 'spawnYaw': 0}, 5: {'spawnPos': (771.5861, 211.0021, 776.5501), 'country': 5, 'spaceUType': 1, 'level': 0, 'moveSpeed': 60, 'gold': 0, 'exp': 0, 'spawnYaw': 0}, 6: {'spawnPos': (771.5861, 211.0021, 776.5501), 'country': 6, 'spaceUType': 1, 'level': 0, 'moveSpeed': 60, 'gold': 0, 'exp': 0, 'spawnYaw': 0}}
 
 allDatas = {
